775e11bf4ac2ed8047ce921de3693d0739d6a42e
Bryan Singh <bhsingh@mitre.org>
2016-03-11 00:02:45 -0500

    Updated paperclip to version 4.0.0, file validation

--- db/migrate/20140702145602_add_resume_to_users.rb
+++ db/migrate/20140702145602_add_resume_to_users.rb
@@ -2,4 +2,10 @@ class AddVerificationToUsers < ActiveRecord::Migration
   def change
     add_attachment :users, :Verification, :Resume
   end
+
+  # def prevent_spoofing
+  # 	add_attachment :avatar
+  # 	add_attachment :avatar, :matches => [/pdf\Z/, /docx\Z/]
+  # 	add_attachment :avatar
+  # end
 end
