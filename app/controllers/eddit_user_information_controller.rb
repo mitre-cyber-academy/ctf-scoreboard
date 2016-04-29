b09fd84260dfc640677024699538d9c33dccc16b
Bryan Singh <bhsingh@mitre.org>
2016-04-29 00:37:37 -0400

    edit contoller to allow user to edit it

--- app/controllers/eddit_user_information_controller.rb
+++ app/controllers/eddit_user_information_controller.rb
@@ -1,4 +1,19 @@
 class EdditUserInformationController < ApplicationController
+	#this is for allowing the user to edit there information
+  def show
+  	#@user = User.find(params[:id])
+  end
+
   def new
   end
+
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  private
+  #this is allowing them to edit there infromation, choosing what info they can edit
+  	def user_params
+  		params.require(:user).permit(:username, :password, :password_confirmation, :remember_me, :team_id, :team_captain, :email, :name, :school, :year_in_school, :gender, :play_for_money,:still_compete ,:age, :area_of_study, :location, :personal_email, :resume, :verification)
+  	end
 end
