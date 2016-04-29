5c3d05f7f02cbee48110ef8cecc93f65a871b97e
Bryan Singh <bryansingh14@gmail.com>
2016-04-29 08:09:17 -0400

    added update and destroy in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,24 @@ class UsersController < ApplicationController
       redirect_to team_path(@team), :alert => @user.errors.full_messages.join(', ')
     end  
   end
+    #will remove user affiliations with current team
+    #only can be part of 1 team at a time
+  def update
+    user = User.where(:id => params[:id])
+    user.update(params[])
+    user.save
+  end
+#remove a user from a team
+#deletes the user entirely
+def destory
+  @user = User.find(params[:id])
+  if @user.destory
+    flash[:notice] = "User was successfully removed."
+  else
+    flash[:error] = "There was a problem with removing the user."
+  end
+  redirect_to teams_path
+end
 
   private
 
