557069bf7ed655d8f0232806e2ac8083de4ad314
Bryan Singh <bhsingh@mitre.org>
2016-04-29 00:28:36 -0400

    added ssl to production enviroment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,6 +37,8 @@ Rails.application.configure do
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
+config.force_ssl = true
+
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
