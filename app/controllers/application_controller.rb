ded89a4690ccf4604cd540bfa85c1dafe414c142
Kyle Fagan <bialogs@gmail.com>
2016-05-23 16:55:14 -0400

    Users can edit their information

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :set_mailer_host
   helper :all
- 
+
   def set_mailer_host
     ActionMailer::Base.default_url_options[:host] = request.host_with_port
   end
@@ -12,5 +12,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters # Be sure to update this in the confirmations controller as well if you need to add parameters.
     devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name, :affiliation, :year_in_school, :state, :compete_for_prizes, :gender, :age, :area_of_study])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:full_name, :affiliation, :year_in_school, :state, :compete_for_prizes, :gender, :age, :area_of_study])
   end
 end
