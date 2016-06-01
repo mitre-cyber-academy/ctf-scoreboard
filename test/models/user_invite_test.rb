9ae07fb3908c2b30d916f18c77ad4cc93df6cc9b
Kyle Fagan <bialogs@gmail.com>
2016-06-01 17:10:41 -0400

    Delete uneccessary creation of a UserInvite in the test

--- test/models/user_invite_test.rb
+++ test/models/user_invite_test.rb
@@ -1,15 +1,6 @@
 require 'test_helper'
 
 class UserInviteTest < ActiveSupport::TestCase
-
-  def setup
-    # create a mailer here and use it to call private functions
-    @user_invite = UserInvite.create!(
-      email: users(:user_two).email,
-      team: teams(:team_one)
-    )
-  end
-
   test "send email" do
     assert_difference 'ActionMailer::Base.deliveries.size', +1 do
       user_invites(:invite_one).send(:send_email)
