3b73d7d6f75089728abd9998b09df08cf444f3f2
Robert Clark <rbclark@mitre.org>
2016-06-03 08:53:03 -0400

    Remove unused routes.

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ CtfRegistration::Application.routes.draw do
 
   # Saying resources :users do causes all routes for the team to be generated. By saying only: [] it keeps only the routes
   # specified in the do block to be generated.
-  resources :teams do
+  resources :teams, except: [:edit, :destroy, :index] do
     # Custom route for accepting user invites.
     resources :user_invites, only: [:destroy] do
       get :accept, on: :member
