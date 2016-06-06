bc59976878c6e8bdf1f3b8c5b6d91b6743df61e2
Kyle Fagan <bialogs@gmail.com>
2016-06-06 07:46:58 -0400

    WIP: testing delete user

--- test/functional/registrations_controller_test.rb
+++ test/functional/registrations_controller_test.rb
@@ -1,12 +1,18 @@
 require 'test_helper'
 
-class UsersControllerTest < ActionController::TestCase
+class RegistrationsControllerTest < ActionController::TestCase
+
   test "unable to destroy team captain" do
+    sign_in users(:user_one)
+    delete :destroy, id: users(:user_one).id
   end
 
   test "destroy user on a team" do
   end
 
   test "destroy user not on a team" do
+    sign_in users(:user_three)
+    delete :destroy
+    assert :success
   end
 end
