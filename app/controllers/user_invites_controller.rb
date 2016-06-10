3830330bb637f89785ba84d869aa6936902d4793
Robert Clark <rbclark@mitre.org>
2016-06-10 14:21:10 -0400

    Fixed accept method in order to avoid ABC complaints from rubocop.

--- app/controllers/user_invites_controller.rb
+++ app/controllers/user_invites_controller.rb
@@ -1,12 +1,11 @@
 class UserInvitesController < ApplicationController
-  before_action :check_access, only: [:destroy]
+  before_action :user_logged_in?
+  before_action :check_accept_access, only: [:accept]
+  before_action :check_destroy_access, only: [:destroy]
 
   # Method for a free agent user to accept a team invitation.
   def accept
-    @user_invite = current_user.user_invites.find(params[:id])
-    if @user_invite.nil?
-      redirect_to :back, alert: I18n.t('invites.invalid_permissions')
-    elsif @user_invite.accept
+    if @user_invite.accept
       redirect_to team_path(@user_invite.team), notice: I18n.t('invites.accepted_successful')
     else
       redirect_to :back, alert: I18n.t('invites.full_team')
@@ -22,8 +21,14 @@ class UserInvitesController < ApplicationController
 
   private
 
-  # Only allow team captain and the user invited to deleted a user invite.
-  def check_access
+  # Only allow user invited to accept a user invite.
+  def check_accept_access
+    @user_invite = current_user.user_invites.find(params[:id])
+    redirect_to :back, alert: I18n.t('invites.invalid_permissions') if @user_invite.nil?
+  end
+
+  # Only allow team captain and the user invited to delete a user invite.
+  def check_destroy_access
     @user_invite = UserInvite.find(params[:id])
     unless @user_invite.user.eql?(current_user) || @user_invite.team.team_captain.eql?(current_user)
       raise ActiveRecord::RecordNotFound
