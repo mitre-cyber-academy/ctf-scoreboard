70509276cf863c3f2a59ac18e60bb8d1bda22ba0
Kyle Fagan <bialogs@gmail.com>
2016-06-13 15:11:44 -0400

    Test stubs

--- test/functional/user_requests_controller_test.rb
+++ test/functional/user_requests_controller_test.rb
@@ -1,4 +1,18 @@
 require 'test_helper'
 
 class UserRequestsControllerTest < ActionController::TestCase
+
+  def setup
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    @request.env["HTTP_REFERER"] = 'http://test.com/'
+  end
+
+  test 'create request' do
+  end
+
+  test 'accept request' do
+  end
+
+  test 'destroy request' do
+  end
 end
