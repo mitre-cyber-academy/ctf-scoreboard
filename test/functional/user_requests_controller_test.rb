5d797b5e90832f4eab678a00730a79820622f31a
Kyle Fagan <bialogs@gmail.com>
2016-06-13 15:32:28 -0400

    WIP: User Request tests

--- test/functional/user_requests_controller_test.rb
+++ test/functional/user_requests_controller_test.rb
@@ -4,15 +4,38 @@ class UserRequestsControllerTest < ActionController::TestCase
 
   def setup
     @request.env["devise.mapping"] = Devise.mappings[:user]
-    @request.env["HTTP_REFERER"] = 'http://test.com/'
+    @request.env["HTTP_REFERER"] = 'http://test.host/'
   end
 
   test 'create request' do
+    sign_in users(:user_two)
+    post :create, team_id: teams(:team_one)
+    assert_redirected_to user_root_path
+    assert_equal I18n.t('requests.sent_successful'), flash[:notice]
+  end
+
+  test 'fail to create request' do
   end
 
   test 'accept request' do
   end
 
-  test 'destroy request' do
+  test 'accept request but user already on a team' do
+  end
+
+  test 'accept request but team now has too many members' do
+  end
+
+  test 'user not able to accept request' do
+  end
+
+  test 'user destroys own request' do
+    user = users(:user_two)
+    delete :destroy, team_id: user_requests(:request_one).team, id: users(:user_two).id
+    assert_redirected_to @request.env["HTTP_REFERER"]
+    assert_equal I18n.t('requests.rejected_sucessful'), flash[:notice]
+  end
+
+  test 'captain destroys user request' do
   end
 end
