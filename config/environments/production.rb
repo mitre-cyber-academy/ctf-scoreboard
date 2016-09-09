64f6cab60e9ec8b7181418d0b52153a6ad4d13a7
Robert Clark <rbclark@mitre.org>
2016-09-09 12:44:59 -0400

    Move over to sendgrid for sending emails from production on heroku.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,11 +84,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   ActionMailer::Base.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    user_name: Rails.application.secrets.gmail_username,
-    password: Rails.application.secrets.gmail_password,
-    authentication: 'plain',
-    enable_starttls_auto: true
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
 end
