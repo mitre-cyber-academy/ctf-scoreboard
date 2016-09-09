b8fa201d9525edadcf28b19261aa26626f61a13e
Robert Clark <rbclark@mitre.org>
2016-09-09 12:51:23 -0400

    Update to the ruby 1.9 syntax for hashes.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,12 +84,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
+    address:             'smtp.sendgrid.net',
+    port:                '587',
+    authentication:      :plain,
+    user_name:           ENV['SENDGRID_USERNAME'],
+    password:            ENV['SENDGRID_PASSWORD'],
+    domain:              'heroku.com',
+    enable_starttls_auto: true
   }
 end
