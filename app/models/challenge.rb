97f2f6449e7883541d01f3f8628649dab9a758ac
Kyle Fagan <bialogs@gmail.com>
2017-04-19 16:36:12 -0400

    Updating relationships for models

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,2 +1,6 @@
 class Challenge < ActiveRecord::Base
+  belongs_to :category
+  has_many :flags, inverse_of: :challenge
+  has_many :solved_challenges
+  has_many :submitted_flags
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,9 @@
 class Game < ActiveRecord::Base
+  has_many :divisions
+  has_many :teams, through: :divisions
+  has_many :feed_items, through: :divisions
+  has_many :achievements, through: :divisions
+  has_many :messages
+  has_many :categories
+  has_many :challenges, through: :categories
 end
