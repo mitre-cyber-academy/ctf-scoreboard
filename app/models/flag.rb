ecf25e7c60b453fe4e13602450de806884c2973b
Kyle Fagan <bialogs@gmail.com>
2017-05-01 20:55:26 -0400

    Moved model code over from ctf-scoreboard for flag, submitted_flag. [ci skip]

--- app/models/flag.rb
+++ app/models/flag.rb
@@ -1,3 +1,21 @@
 class Flag < ActiveRecord::Base
   belongs_to :challenge, inverse_of: :flags
+
+  validates :flag, presence: true
+
+  def invoke_api_request
+    # Make API call if one is specified. This throws away any failed requests
+    # since we really don't care that much if the actions really happen. The
+    # player properly being credited their points is more important.
+    Thread.new do
+      begin
+        Net::HTTP.get(URI.parse(api_request)) if api_request
+      # Could list all the execeptions, most advice advocates to move
+      # to a 3rd party HTTP library that has errors derived from a common
+      # superclass
+      rescue StandardError
+        logger.info "Error invoking api request for flag submission #{self}"
+      end
+    end
+  end
 end
