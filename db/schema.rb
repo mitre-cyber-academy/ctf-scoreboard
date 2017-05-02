b2ed648bd990579ff5b0ca7b10652e4f42cd4c2f
Robert Clark <rbclark@mitre.org>
2017-05-01 21:57:01 -0400

    [ci skip] WIP: Fix misnamed table and other misc fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170502001124) do
+ActiveRecord::Schema.define(version: 20170502015437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,7 +65,7 @@ ActiveRecord::Schema.define(version: 20170502001124) do
     t.string  "type"
   end
 
-  create_table "flagsses", force: :cascade do |t|
+  create_table "flags", force: :cascade do |t|
     t.integer "challenge_id"
     t.string  "flag"
     t.string  "api_url"
