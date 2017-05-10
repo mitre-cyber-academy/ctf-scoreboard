6e875624ef2ecd40bcf7a451a52d3aea84403eae
Robert Clark <rbclark@mitre.org>
2017-05-09 23:29:58 -0400

    Remove Index method from games model since it is unused

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,14 +8,5 @@ class GamesController < ApplicationController
     @solved_challenges = current_user&.team&.solved_challenges
   end
 
-  def index
-    @game = Game.instance
-    @categories = @game.categories.includes(:challenges).order(:name)
-    @challenges = @game.challenges
-    @divisions = @game.divisions
-    # Only exists for the purpose of providing an active tab for admins.
-    @active_division = @divisions.first
-  end
-
   def summary; end
 end
