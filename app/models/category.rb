70d1fffa2bbb8a7fb5e9e0ded386948db5069379
Kyle Fagan <kfagan@mitre.org>
2017-05-09 23:17:31 -0400

    Fixed no method 'at' by using [] to access array and simplified test code [ci skip]

--- app/models/category.rb
+++ app/models/category.rb
@@ -12,6 +12,6 @@ class Category < ActiveRecord::Base
   def next_challenge(challenge)
     # Order of challenges is handled by default scope in challenge.rb
     index = challenges.find_index(challenge)
-    challenges.at(index + 1) unless index.nil?
+    challenges[index + 1] unless index.nil?
   end
 end
