f80d47e9a0e8ece733c5c8dd46b5215d124fc867
Robert Clark <rbclark@mitre.org>
2017-05-10 16:51:43 -0400

    Continue updating gameboard and add countdown timer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,7 @@
 # Controller for the homepage. Since there is not much logic on the homepage this controller is
 # rather empty.
 class HomeController < ApplicationController
-  def index; end
+  def index
+    @game = Game.instance
+  end
 end
