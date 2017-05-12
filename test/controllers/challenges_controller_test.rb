7c3ec33ed2dc0c1d9737c1ea3dbd771493ef8e1f
Kyle Fagan <kfagan@mitre.org>
2017-05-12 10:25:52 -0400

    Added new challenges controller tests

--- test/controllers/challenges_controller_test.rb
+++ test/controllers/challenges_controller_test.rb
@@ -1,36 +1,48 @@
 require 'test_helper'
 
 class ChallengesControllerTest < ActionController::TestCase
+  include ChallengesHelper
 
   def setup
     @request.env["devise.mapping"] = Devise.mappings[:user]
   end
 
-  test 'index' do
+  test 'should get show' do
     sign_in users(:user_one)
-    # Render
-    get :index
-    assert :success
-  end
-
-  test "should get show" do
-    get :show
-    assert_response :success
-  end
-
-  test "should get submit_flag" do
-    get :submit_flag
-    assert_response :success
-  end
-
-  test "should get find_challenge" do
-    get :find_challenge
+    get :show, params: {
+      id: challenges(:challenge_one_cat_one)
+    }
     assert_response :success
   end
 
   test 'submit correct flag' do
+    sign_in users(:user_one)
+    put :update, params: {
+      id: challenges(:challenge_one_cat_one),
+      challenge: {
+        submitted_flag: flags(:flag_one).flag
+      }
+    }
+    assert :success
+    assert_equal flash[:notice], I18n.t('flag.accepted')
   end
 
   test 'submit incorrect flag' do
+    sign_in users(:user_one)
+    put :update, params: {
+      id: challenges(:challenge_one_cat_one),
+      challenge: {
+        submitted_flag: "wrong"
+      }
+    }
+    assert :success
+    assert true, wrong_flag_messages.include?(flash[:notice])
+  end
+
+  # Private methods
+  test 'should get find_challenge' do
+  end
+
+  test 'should find and log flag' do
   end
 end
