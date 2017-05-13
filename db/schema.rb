aa5a350d9ba988b2582e118a57a640a29b019048
Robert Clark <rbclark@mitre.org>
2017-05-13 17:07:53 -0500

    Added graphics to summary page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170512141018) do
+ActiveRecord::Schema.define(version: 20170513211713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,9 +111,11 @@ ActiveRecord::Schema.define(version: 20170512141018) do
   end
 
   create_table "submitted_flags", force: :cascade do |t|
-    t.integer "challenge_id"
-    t.integer "user_id"
-    t.string  "text"
+    t.integer  "challenge_id"
+    t.integer  "user_id"
+    t.string   "text"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "teams", force: :cascade do |t|
