3a8d230a18ff87e545c16b318785f9b5f8f80af1
Robert Clark <rbclark@mitre.org>
2017-05-21 15:49:58 -0400

    Lock many aspects of the game from viewing until the game is open.

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class AchievementsController < ApplicationController
-  before_action :load_game, :load_message_count
+  before_action :load_game, :filter_access_before_game_open, :load_message_count
   include ActionView::Helpers::TextHelper
 
   def index
