065542b5c453f4ed0928562d1ee93a9331ece116
Robert Clark <rbclark@mitre.org>
2017-05-30 09:54:25 -0400

    Add APP_URL ENV so production mailer works

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,9 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.default_url_options = { host: ENV['APP_URL'] }
+
   ActionMailer::Base.smtp_settings = {
     address:             'smtp.sendgrid.net',
     port:                '587',
