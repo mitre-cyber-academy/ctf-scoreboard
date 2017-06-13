c8f7c34496c5631280e55f60d78c05df6ebc57ad
James Tobin <jtobin@mitre.org>
2017-06-13 15:12:48 -0400

    Created emails for...
    reminding users about the competition (has countdown to competition)
    congratulating users for taking part in the competition and shows ranking (assumes user is on team)
    congratulating user in top ten and asks for resume (assumes user is on team in top ten)
    added methods to user_mailer to send these emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -13,4 +13,25 @@ class UserMailer < ApplicationMailer
     @user = user_request.user
     mail(to: @captain.email, subject: "MITRE CTF: Request from #{@user.full_name} to join #{@team.team_name}")
   end
+
+  def competition_reminder(user)
+    @user = user
+    @game = Game.instance
+    mail(to: @user.email, subject: 'MITRE CTF: Competition Reminder')
+  end
+
+  # Assumes user is on a team
+  def ranking(user)
+    @user = user
+    @div = user.team.division
+    @rank = 1 + (@div.ordered_teams.index user.team)
+    mail(to: @user.email, subject: 'MITRE CTF: Competition Ranking')
+  end
+
+  # Assumes user is in top ten
+  def resume(user)
+    @user = user
+    @end_time = Game.instance.stop + 2.weeks
+    mail(to: @user.email, subject: 'MITRE CTF: Resume Request')
+  end
 end
