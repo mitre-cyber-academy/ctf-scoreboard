041fdfaf69ca6c39d5e66e5395c5a90c2c049360
Robert Clark <rbclark@mitre.org>
2018-03-07 12:45:24 -0500

    Set default stub for geocoder during testing in order to avoid API limit errors

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,3 +1,16 @@
 Geocoder.configure(
   timeout: 15
 )
+
+if Rails.env.test?
+  Geocoder.configure(:lookup => :test)
+
+  Geocoder::Lookup::Test.set_default_stub(
+    [
+      {
+        'country'      => 'United States',
+        'country_code' => 'US'
+      }
+    ]
+  )
+end
