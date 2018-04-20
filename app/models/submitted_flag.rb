15b8a34a9d375b55f0c8c32c776c7ef04184638a
James Tobin <jtobin@mitre.org>
2018-04-20 18:28:32 -0400

    Adds validation for user, team, and challenge to solve #104

--- app/models/submitted_flag.rb
+++ app/models/submitted_flag.rb
@@ -6,4 +6,12 @@ class SubmittedFlag < ApplicationRecord
   belongs_to :team
 
   validates :text, presence: true
+  validates :user_id, presence: true
+  validates :challenge_id, presence: true
+
+  before_validation :on_a_team?
+
+  def on_a_team?
+    throw :abort unless user&.on_a_team?
+  end
 end
