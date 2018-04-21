48f34ee40db4814d8641dc6e68642eab2fbee627
James Tobin <jtobin@mitre.org>
2018-04-20 20:48:31 -0400

    Remove validations and move tests.
    Turns out validation was done somewhere else.  Tests modified to work through challenges controller.

--- app/models/submitted_flag.rb
+++ app/models/submitted_flag.rb
@@ -6,12 +6,4 @@ class SubmittedFlag < ApplicationRecord
   belongs_to :team
 
   validates :text, presence: true
-  validates :user_id, presence: true
-  validates :challenge_id, presence: true
-
-  before_validation :on_a_team?
-
-  def on_a_team?
-    throw :abort unless user&.on_a_team?
-  end
 end
