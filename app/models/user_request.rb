dbc9686931e6a58c013ce835e5fc47fd2534b277
Robert Clark <rbclark@mitre.org>
2019-12-10 11:36:28 -0500

    Fix rubocop autocorrect

--- app/models/user_request.rb
+++ app/models/user_request.rb
@@ -5,7 +5,7 @@ class UserRequest < ApplicationRecord
   belongs_to :team, optional: false
   belongs_to :user, optional: false
 
-  enum status: { Pending => 0, Accepted => 1, Rejected => 2 }
+  enum status: { Pending: 0, Accepted: 1, Rejected: 2 }
 
   after_create :send_email
 
