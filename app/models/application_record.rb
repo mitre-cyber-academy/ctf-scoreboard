0a2e28e78ca59391310d62b74b26e9781a719c65
Kyle Fagan <kfagan@mitre.org>
2020-01-17 17:28:49 -0500

    Update geocoding test. Create test for linking invites with users, remove transformation code and test cruft.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,14 +2,4 @@
 
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
-  def self.transform(name)
-    temp = name.dup
-    temp.downcase!
-    temp.tr! ' ', '_'
-    temp.tr! '@', 'a'
-    temp.tr! '$', 's'
-    temp.gsub!(/[^a-z0-9_]/, '')
-    temp
-  end
 end
