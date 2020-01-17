41971f9aaaca55ef4c8053141ff2e14c3e63d908
Kyle Fagan <kfagan@mitre.org>
2020-01-17 18:24:16 -0500

    Remove old redirect back to page after sign in methodology that is not supported or implemented within devise/our app

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,17 +2,6 @@
 
 module SessionsHelper
   def deny_access
-    store_location
     redirect_to new_user_session_path, notice: I18n.t('users.login_required')
-  end
-
-  private
-
-  def store_location
-    session[:return_to] = request.fullpath
-  end
-
-  def clear_stored_location
-    session[:return_to] = nil
   end
 end
