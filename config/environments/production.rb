57ccbf9f9da3175aef0b8fe99959ef4c8e1d393b
Kyle Fagan <kfagan@mitre.org>
2020-01-22 14:25:32 -0500

    Add email rake task for configuring ActionMailer upon setup. Remove ActionMailer configurations from production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,14 +96,4 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.default_url_options = { host: ENV['APP_URL'] }
-
-  ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: '587',
-    authentication: :plain,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
-    enable_starttls_auto: true
-  }
 end
