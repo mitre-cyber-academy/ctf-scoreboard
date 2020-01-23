d813825345c13462618bbc478659dc98ad155185
Kyle Fagan <kfagan@mitre.org>
2020-01-23 11:30:03 -0500

    Update reCAPTCHA integration to use before_action. Update target Ruby version to 2.6 in rubocop and fix some errors caused by changing that.

--- app/models/flag.rb
+++ app/models/flag.rb
@@ -19,14 +19,12 @@ class Flag < ApplicationRecord
     # since we really don't care that much if the actions really happen. The
     # player properly being credited their points is more important.
     Thread.new do
-      begin
-        Net::HTTP.get(URI.parse(api_request)) if api_request
+      Net::HTTP.get(URI.parse(api_request)) if api_request
       # Could list all the execeptions, most advice advocates to move
       # to a 3rd party HTTP library that has errors derived from a common
       # superclass
-      rescue StandardError
-        logger.info "Error invoking api request for flag submission #{self}"
-      end
+    rescue StandardError
+      logger.info "Error invoking api request for flag submission #{self}"
     end
   end
 end
