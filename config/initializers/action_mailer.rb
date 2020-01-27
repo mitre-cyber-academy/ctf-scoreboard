ac5e2005751b45fb4646049ed89442541a28fdce
Kyle Fagan <kfagan@mitre.org>
2020-01-27 16:04:57 -0500

    Check for nil before cast to symbol in action mailer config

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   address: ENV['MAILER_ADDRESS'],
   port: ENV['MAILER_PORT'],
   domain: ENV['MAILER_DOMAIN'],
-  authentication: ENV['MAILER_AUTHENTICATION'].to_sym,
+  authentication: ENV['MAILER_AUTHENTICATION']&.to_sym,
   tls: ActiveModel::Type::Boolean.new.cast(ENV['MAILER_TLS']),
   openssl_verify_mode: ENV['MAILER_OPENSSL_VERIFY_MODE'],
   enable_starttls_auto: ActiveModel::Type::Boolean.new.cast(ENV['MAILER_ENABLE_STARTTLS_AUTO']),
