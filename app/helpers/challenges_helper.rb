ce40f69910c17597ea5e8730ce6a929bdae48459
Kyle Fagan <kfagan@mitre.org>
2020-02-05 11:45:41 -0500

    Refactor the game summary page

--- app/helpers/challenges_helper.rb
+++ app/helpers/challenges_helper.rb
@@ -18,4 +18,26 @@ module ChallengesHelper
     youtube_id = youtube_url.split('v=').last
     content_tag(:iframe, nil, src: "https://www.youtube.com/embed/#{youtube_id}?rel=0", frameborder: 0)
   end
+
+  def submit_url(challenge)
+    return game_team_challenge_path(@defense_team, challenge.challenge_id) if challenge.is_a?(Flag)
+
+    game_challenge_path(challenge)
+  end
+
+  def subheading(team, challenge)
+    if team
+      "#{team.team_name} - #{challenge.point_value(current_user&.team)} points"
+    else
+     "#{challenge.category.name} - #{challenge.point_value} points"
+    end
+  end
+
+  def admin_edit_url(challenge)
+    if challenge.is_a?(Flag)
+      rails_admin.edit_path("pentest_challenge", challenge)
+    else
+      rails_admin.edit_path("point_challenge", challenge)
+    end
+  end
 end
---
+++ app/helpers/games_helper.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+module GamesHelper
+  def table_subtitle(game, challenges, headings)
+    noun = team_or_category(game)
+    "#{pluralize(challenges.size, 'challenge')} across #{pluralize(headings.size, noun)}"
+  end
+
+  def team_or_category(game)
+    return 'team' if game.is_a?(PentestGame)
+
+    'category'
+  end
+
+  def heading_width(headings)
+    1.0 / headings.size.to_f * 100.0
+  end
+
+  def cell_background_color(team)
+    return '' unless team.eql? current_user&.team&.team_name
+
+    'background-color: #F0F0F0;'
+  end
+
+  def challenge_color(challenge, defense_team)
+   return 'color:#999999;' if own_team_challenge?(defense_team)
+   if challenge.force_closed?
+     'color:#800000;'
+   elsif challenge.get_solved_challenge_for(current_user&.team)
+     'color:#00abca;'
+   elsif challenge.solved?(2)
+     'color:#00cc00;'
+   elsif challenge.solved?(1)
+     'color:#009900;'
+   elsif challenge.open?
+     'color:#006600;'
+   else
+     'color:#999999;'
+    end
+  end
+
+  def own_team_challenge?(defense_team)
+    defense_team&.eql?(current_user&.team)
+  end
+end
