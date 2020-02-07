0d4557bbd2c6195e30b79d7439b7191accceb5e7
Kyle Fagan <kfagan@mitre.org>
2020-02-07 16:49:51 -0500

    Refactor team and user views

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -6,4 +6,22 @@ module TeamsHelper
 
     "#{team.team_name} (ineligible)"
   end
+
+  def category_header(game)
+    return 'Defense Team' if game.is_a?(PentestGame)
+
+    'Category'
+  end
+
+  def solved_challenge_title(game, solved_challenge)
+    solved_challenge.flag.team.team_name if game.is_a?(PentestGame)
+
+    solved_challenge.challenge.category.name
+  end
+
+  def eligible?(request)
+    t('teams.show.eligible') if request.user.compete_for_prizes
+
+    t('teams.show.ineligible')
+  end
 end
