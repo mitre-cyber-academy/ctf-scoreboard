edc6ff3855ce2171b32dd8dd9d8db57a5320c469
Kyle Fagan <kfagan@mitre.org>
2020-02-17 16:35:02 -0500

    Remove tests for cruft feeditem code

--- test/models/feed_item_test.rb
+++ test/models/feed_item_test.rb
@@ -7,15 +7,4 @@ class FeedItemTest < ActiveSupport::TestCase
     @solved_challenge = create(:point_solved_challenge, challenge: create(:point_challenge, category: create(:category, game: @game)))
     @score_adjustment = create(:point_score_adjustment)
   end
-
-  test 'description' do
-    assert_includes @achievement.description, @achievement.class.name
-    assert_includes @solved_challenge.description, @solved_challenge.challenge.category.name
-  end
-
-  test 'icon' do
-    assert_equal @solved_challenge.icon, 'ok'
-    assert_equal @achievement.icon, 'certificate'
-    assert_equal @score_adjustment.icon, 'chevron-up'
-  end
 end
