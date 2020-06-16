a0cf046afa4f54c4476e378b0e3c3178fee5cecc
Sheikholeslam Sean <sheikholeslam@mitre.org>
2020-06-16 11:40:17 -0400

    JS Function for Enhancement Issues #441 and #440

--- app/assets/javascripts/challenges.js
+++ app/assets/javascripts/challenges.js
@@ -1,2 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+// Purpose: Provides show and hide feature to a clickable element with "toggler" class
+// Date: 6/16/2020
+
+$(document).ready(function() {
+    $(".toggler").click(function(e) {
+        e.preventDefault();
+      $(".hide").toggle("hide");
+    });
+  });
