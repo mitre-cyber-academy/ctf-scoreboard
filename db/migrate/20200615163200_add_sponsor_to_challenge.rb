b6143c4b4d4b9d8d14c828353ee763651642e46d
camdenmoors <66680985+camdenmoors@users.noreply.github.com>
2020-06-17 08:30:43 -0400

    Combine migration into one file
    Combining migrations into one file so there's less files and so CI tests will work.

--- db/migrate/20200615163200_add_sponsor_to_challenge.rb
+++ db/migrate/20200615163200_add_sponsor_to_challenge.rb
@@ -2,5 +2,7 @@ class AddSponsorToChallenge < ActiveRecord::Migration[6.0]
   def change
     add_column :challenges, :sponsored, :boolean, default: false, null: false
     add_column :challenges, :sponsor, :text, default: ''
+    add_column :challenges, :sponsor_logo, :text, default: '', :null => false
+    add_column :challenges, :sponsor_description, :text, default: '', :null => false
   end
 end
--- db/migrate/20200617120116_add_sponsor_logo.rb
+++
@@ -1,6 +0,0 @@
-class AddSponsorLogo < ActiveRecord::Migration[6.0]
-  def change
-    add_column :challenges, :sponsor_logo, :text, default: '', :null => false
-    add_column :challenges, :sponsor_description, :text, default: '', :null => false
-  end
-end
