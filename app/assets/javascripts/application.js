0a99383d7fa30205d1458840840dd982269d12af
sesheikholeslam <sheikholeslam@mitre.org>
2020-06-19 16:09:20 -0400

    Commit to Rerun Test Enviroment

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,5 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
+// listed below. 
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
