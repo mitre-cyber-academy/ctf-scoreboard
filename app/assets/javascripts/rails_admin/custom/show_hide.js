d861df5d7d5bb104a3cb305d1b7ae0b18704faa1
camdenmoors <66680985+camdenmoors@users.noreply.github.com>
2020-06-22 16:10:00 -0400

    Switch to using showOrHide for all show/hides

--- app/assets/javascripts/rails_admin/custom/show_hide.js
+++ app/assets/javascripts/rails_admin/custom/show_hide.js
@@ -1,4 +1,5 @@
 $(document).on('rails_admin.dom_ready', function() {
+
   // Run if we're in the challenge page
   if($('.sponsorship_fields_toggler').length){
     toggleSponsorField()
@@ -12,27 +13,27 @@ $(document).on('rails_admin.dom_ready', function() {
       document.getElementById('game_prizes_available').addEventListener('change', toggleCertAndPrizes);
   }
 
-  // Show or hide sponsorship info based on sponsored tickbox
+  // Wrapper for all showing/hiding of sponsors
   function toggleSponsorField() {
-      var elems = $('.sponsorship_fields_toggle')
-      if ($('.sponsorship_fields_toggler').find('input')[1].checked) {
-          elems.show()
-      } else {
-          elems.hide()
-      }
+    var checkbox_id = $('.sponsorship_fields_toggler').find('input')[1].id
+    showOrHide(checkbox_id, 'standard_challenge_sponsor_field')
+    showOrHide(checkbox_id, 'standard_challenge_sponsor_logo_field')
+    showOrHide(checkbox_id, 'standard_challenge_sponsor_description_field')
   }
 
-  // Show or hide completion certificates based on certificates tickbox
+  // Wrapper for all showing/hiding of game prizes/completion certificates
   function toggleCertAndPrizes() {
-      if (document.getElementById('game_enable_completion_certificates').checked) {
-          document.getElementById('game_completion_certificate_template_field').style.display = "";
-      } else {
-          document.getElementById('game_completion_certificate_template_field').style.display = "none";
-      }
-      if (document.getElementById('game_prizes_available').checked) {
-        document.getElementById('game_prizes_text_field').style.display = "";
-      } else {
-        document.getElementById('game_prizes_text_field').style.display = "none";
+    showOrHide('game_enable_completion_certificates', 'game_completion_certificate_template_field')
+    showOrHide('game_prizes_available', 'game_prizes_text_field')
+  }
+
+  // Show or hide based off a checkbox
+  function showOrHide(checkboxId, fieldToToggle){
+    if(document.getElementById(checkboxId).checked){
+      document.getElementById(fieldToToggle).style.display = "";
+    }
+    else{
+      document.getElementById(fieldToToggle).style.display = "none";
     }
   }
 });
