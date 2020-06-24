3f0ea6bbc77202200e080a73ee586afc6264cbba
Camden Moors <cmoors@mitre.org>
2020-06-24 13:51:26 -0400

    Ensure that the game actually closes when its past the stop date

--- test/models/challenge_test.rb
+++ test/models/challenge_test.rb
@@ -11,4 +11,11 @@ class ChallengeTest < ActiveSupport::TestCase
     assert_equal chal2, chal1.next_challenge
     assert_nil chal3.next_challenge
   end
+
+  test 'game is closed when its passed closing time' do
+    game = create(:active_game)
+    challenge = create(:standard_challenge, challenge_end: Time.current - 1.days)
+
+    assert_not challenge.before_close?
+  end
 end
