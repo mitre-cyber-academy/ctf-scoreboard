dfd340554a27b53717cc79563a545e56cf803067
Camden Moors <cmoors@mitre.org>
2020-06-25 16:50:11 -0400

    Create function for defining game and only use it on new session page.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,9 @@
 
 class SessionsController < Devise::SessionsController
   before_action :load_game, :load_message_count, except: %i[new destroy]
-  before_action { @game = Game.instance }
+  before_action :define_game, only: %i[new]
+
+  def define_game
+    @game = Game.instance
+  end
 end
