70e4f65319c60274c9e3708417465fe66b58b91b
sesheikholeslam <sheikholeslam@mitre.org>
2020-06-26 13:11:34 -0400

    Moves Looking for Members Checkbox to Edit Team Page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_23_173255) do
+ActiveRecord::Schema.define(version: 2020_06_26_145909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,9 +49,9 @@ ActiveRecord::Schema.define(version: 2020_06_23_173255) do
     t.boolean "design_phase", default: false
     t.bigint "game_id"
     t.boolean "sponsored", default: false, null: false
-    t.text "sponsor", default: ''
-    t.text "sponsor_logo", default: '', null: false
-    t.text "sponsor_description", default: '', null: false
+    t.text "sponsor", default: ""
+    t.text "sponsor_logo", default: "", null: false
+    t.text "sponsor_description", default: "", null: false
     t.index ["game_id"], name: "index_challenges_on_game_id"
   end
 
@@ -187,6 +187,7 @@ ActiveRecord::Schema.define(version: 2020_06_23_173255) do
     t.integer "division_id"
     t.boolean "eligible", default: false
     t.integer "slots_available", default: 0
+    t.boolean "looking_for_members", default: true, null: false
     t.index "lower((team_name)::text)", name: "index_teams_on_team_name_unique", unique: true
     t.index ["division_id"], name: "index_teams_on_division_id"
     t.index ["team_captain_id"], name: "index_teams_on_team_captain_id"
