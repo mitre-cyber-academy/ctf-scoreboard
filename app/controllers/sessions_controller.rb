3070bedbc7d01d44dfb0f92275d09ba1a0958b05
Camden Moors <cmoors@mitre.org>
2020-06-26 10:53:13 -0400

    Load load_game_for_login when visiting login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,13 @@
 
 class SessionsController < Devise::SessionsController
   before_action :load_game, :load_message_count, except: %i[new destroy]
-  before_action :define_game, only: %i[new]
+  before_action :load_game_for_login, only: %i[new]
 
-  def define_game
+  def load_game_for_login
     @game = Game.instance
+  end
+
+  def new
+    super
   end
 end
