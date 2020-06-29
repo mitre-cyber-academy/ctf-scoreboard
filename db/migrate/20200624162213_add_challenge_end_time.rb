7983ec950c832efcb7b870cd2f2232251974bd6a
Camden Moors <cmoors@mitre.org>
2020-06-29 12:47:38 -0400

    Remove two unneeded migrations

--- db/migrate/20200624162213_add_challenge_end_time.rb
+++
@@ -1,5 +0,0 @@
-class AddChallengeEndTime < ActiveRecord::Migration[6.0]
-  def change
-    add_column :challenges, :challenge_end, :datetime
-  end
-end
--- db/migrate/20200626140517_remove_challenge_end.rb
+++
@@ -1,5 +0,0 @@
-class RemoveChallengeEnd < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :challenges, :challenge_end
-  end
-end
