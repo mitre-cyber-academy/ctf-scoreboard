1e0c629063c770096f86ad451a74189087c63d4a
Camden Moors <cmoors@mitre.org>
2020-07-01 13:42:59 -0400

    Fix messages test

--- test/integration/message_display_modes_test.rb
+++ test/integration/message_display_modes_test.rb
@@ -1,16 +1,22 @@
 require 'test_helper'
 
-class ApplicationDisplayModesTest < ActionDispatch::IntegrationTest
+class MessageDisplayModesTest < ActionDispatch::IntegrationTest
   include TeamsHelper
   include Devise::Test::IntegrationHelpers
+
+  def setup
+    @game = create(:active_game)
+  end
 
   test 'messages header shows on messages page' do
     sign_in create(:user)
     get "/game/messages"
+
     assert_select 'h1', /Messages/
   end
 
   test 'no messages shows when there are no messages' do
+    @game.messages.first.destroy
     sign_in create(:user)
     get "/game/messages"
     assert_select 'h4', /No Messages/
