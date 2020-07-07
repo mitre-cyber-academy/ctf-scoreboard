dd05625962abbc9f3ab0a41bb4f652ea4312a2cf
Camden Moors <cmoors@mitre.org>
2020-07-07 14:06:48 -0400

    Remove active_navbar, it's not needed anymore

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,10 +23,6 @@ module ApplicationHelper
     num_unread unless num_unread&.eql? 0
   end
 
-  def active_navbar?(page)
-    'active' if current_page? page
-  end
-
   def amount_of_errors(alerts)
     pluralize(alerts.length, I18n.t('challenges.error_singular')) + ':'
   end
