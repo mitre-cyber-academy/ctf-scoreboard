55b81f737500d963bf0a126a96fa5a90ce21f840
camdenmoors <66680985+camdenmoors@users.noreply.github.com>
2020-07-09 11:00:01 -0400

    Fix viewing team page on rails admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -472,7 +472,7 @@ RailsAdmin.config do |config|
     list do
       field :team_name
       field :affiliation
-      field :team_captian
+      field :team_captain
       field :division
     end
   end
