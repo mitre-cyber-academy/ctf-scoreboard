79a5023d4d306f265b47730e83bad3dab26baece
Camden Moors <cmoors@mitre.org>
2020-07-23 11:43:08 -0500

    Set SECRET_KEY_BASE in .env-prod

--- setup-docker-secrets.sh
+++ setup-docker-secrets.sh
@@ -6,7 +6,16 @@ else
 	(umask 066; touch .env)
 	cat >.env - << EOF
 POSTGRES_PASSWORD=$(openssl rand -hex 33)
+SECRET_KEY_BASE=$(openssl rand -hex 64)
+EOF
+fi
 
+if [ -f .env-prod ]; then
+	echo ".env-prod already exists, if you would like to regenerate your secrets, please delete this file and re-run the script."
+else
+	echo ".env-prod does not exist, creating..."
+	cat >.env-prod - << EOF
+SECRET_KEY_BASE=$(openssl rand -hex 64)
 EOF
 fi
 
