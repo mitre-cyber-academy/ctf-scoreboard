8e4b0688c98d98b6494bf3f05a17b63483f97b6a
Camden Moors <cmoors@mitre.org>
2020-07-30 14:28:54 -0500

    Automatically use production environment and use new rake

--- setup.sh
+++ setup.sh
@@ -3,27 +3,13 @@ while [ "$1" != "" ]; do
     case $1 in
         -s | --setupdb )       setupdb=0
                                 ;;
-        -p | --production )    production=0
-                                ;;
     esac
     shift
 done
 
-bundle install
-echo
-
 if [[ "$setupdb" == 0 ]];
 then
-    if [[ "$production" == 0 ]];
-    then
-        rails db:environment:set RAILS_ENV=production
-    else
-        rails db:environment:set RAILS_ENV=development
-    fi
-    echo "Creating tables..."
-    rails db:create
-    echo "Loading schema..."
-    rails db:schema:load
-    echo "Creating administrator user... Please fill out the credentials you want to use to login to the admin account."
-    rails db:create_admin
+    rails db:environment:set RAILS_ENV=production
+    echo "Setting up database..."
+    rails db:initial_setup
 fi
