bdb4dbfcc7bf822f6cf06d42a4d92a06c20c5d2e
Camden Moors <cmoors@mitre.org>
2020-08-13 13:24:37 -0400

    Generate self-signed ssl certificates with setup-docker-secrets.sh

--- setup-docker-secrets.sh
+++ setup-docker-secrets.sh
@@ -19,4 +19,22 @@ SECRET_KEY_BASE=$(openssl rand -hex 64)
 EOF
 fi
 
+if [ -f ./nginx/certs/ssl_certificate.crt ]; then
+	echo "SSL Certificate already exists. if you would like to regenerate your secrets, please delete this file and re-run the script."
+else
+	echo "SSL Certificate does not exist, creating self-signed certificate..."
+	echo "Do not use a self-signed certificate in a production environment."
+	echo
+	echo "Generating certificate signing request..."
+	sudo openssl req -new > temp.cert.csr
+	echo "Generating signing key..."
+	sudo openssl rsa -in privkey.pem -out new.cert.key
+	echo "Generating certificate (Expires in 7 days)..."
+	sudo openssl x509 -in temp.cert.csr -out new.cert.cert -req -signkey new.cert.key -days 7 # 7 Days so it isn't used in production
+	echo "Moving files and cleaning up..."
+	mv -f new.cert.cert nginx/certs/ssl_certificate.crt
+	mv -f new.cert.key nginx/certs/ssl_certificate_key.key
+	rm temp.cert.csr
+fi
+
 echo "Done"
