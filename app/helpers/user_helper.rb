5cf43fddbb10bcb05d5c04d976f7dc432d0058cb
Robert Clark <rbclark@mitre.org>
2021-09-22 14:03:07 -0400

    Use version of filteriffic that is compatible with jquery 3 (#759)

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -14,64 +14,17 @@ module UserHelper
     ]
   end
 
-  # rubocop:disable Metrics/MethodLength
-  def state_enum
-    [
-      %w[Alabama AL],
-      %w[Alaska AK],
-      %w[Arizona AZ],
-      %w[Arkansas AR],
-      %w[California CA],
-      %w[Colorado CO],
-      %w[Connecticut CT],
-      %w[Delaware DE],
-      ['District of Columbia', 'DC'],
-      %w[Florida FL],
-      %w[Georgia GA],
-      %w[Hawaii HI],
-      %w[Idaho ID],
-      %w[Illinois IL],
-      %w[Indiana IN],
-      %w[Iowa IA],
-      %w[Kansas KS],
-      %w[Kentucky KY],
-      %w[Louisiana LA],
-      %w[Maine ME],
-      %w[Maryland MD],
-      %w[Massachusetts MA],
-      %w[Michigan MI],
-      %w[Minnesota MN],
-      %w[Mississippi MS],
-      %w[Missouri MO],
-      %w[Montana MT],
-      %w[Nebraska NE],
-      %w[Nevada NV],
-      ['New Hampshire', 'NH'],
-      ['New Jersey', 'NJ'],
-      ['New Mexico', 'NM'],
-      ['New York', 'NY'],
-      ['North Carolina', 'NC'],
-      ['North Dakota', 'ND'],
-      %w[Ohio OH],
-      %w[Oklahoma OK],
-      %w[Oregon OR],
-      %w[Pennsylvania PA],
-      ['Puerto Rico', 'PR'],
-      ['Rhode Island', 'RI'],
-      ['South Carolina', 'SC'],
-      ['South Dakota', 'SD'],
-      %w[Tennessee TN],
-      %w[Texas TX],
-      %w[Utah UT],
-      %w[Vermont VT],
-      %w[Virginia VA],
-      %w[Washington WA],
-      ['West Virginia', 'WV'],
-      %w[Wisconsin WI],
-      %w[Wyoming WY]
-    ]
+  def country_enum
+    countries = ISO3166::Country.all.map do |country|
+      [country.name, country.alpha2]
+    end
+
+    countries.sort.sort_by { |_name, code| code == 'US' ? 0 : 1 }
   end
-  # rubocop:enable Metrics/MethodLength
+
+  def state_enum
+    ISO3166::Country.find_country_by_name('United States').states.map { |key, struct| [struct.name, key] }
+  end
 
   def required_for_update?(on_update_page)
     return 'control-label' if on_update_page
