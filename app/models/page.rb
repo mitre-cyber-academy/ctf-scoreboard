1cda616719fbee1bbb83ffcafa95149820ba0812
Drew Blumenthal <drewblu123@outlook.com>
2022-02-07 19:06:31 -0500

    resolved lint offenses related to pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 class Page < ApplicationRecord
   belongs_to :game, optional: false
 
-
-  validates :path, presence: true, uniqueness: { scope: :game_id, case_sensitive: false }, :format => { with: /\A[a-zA-Z\-]*\z/ , :message => 'path can only consist of letters and dashes' }
+  validates :path, presence: true, uniqueness: { scope: :game_id, case_sensitive: false },
+    format: { with: /\A[a-zA-Z\-]*\z/, message: 'path can only consist of letters and dashes' }
   before_save :downcase_path
 
   def preview
-    self.body
+    body
   end
 
   def downcase_path
-    self.path.downcase!
+    path.downcase!
   end
 end
