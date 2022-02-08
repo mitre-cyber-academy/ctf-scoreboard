ef9963d3829381f1844d4f91fb7a859a71b58e05
Drew Blumenthal <drewblu123@outlook.com>
2022-02-07 19:18:29 -0500

    resolved last (hopefully) lint offense

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,8 +3,10 @@
 class Page < ApplicationRecord
   belongs_to :game, optional: false
 
-  validates :path, presence: true, uniqueness: { scope: :game_id, case_sensitive: false },
-    format: { with: /\A[a-zA-Z\-]*\z/, message: 'path can only consist of letters and dashes' }
+  validates :path,
+            presence: true,
+            uniqueness: { scope: :game_id, case_sensitive: false },
+            format: { with: /\A[a-zA-Z\-]*\z/, message: 'path can only consist of letters and dashes' }
   before_save :downcase_path
 
   def preview
